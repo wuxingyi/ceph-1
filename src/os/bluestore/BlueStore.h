liewegas/ceph

os/bluestore: _dump_bnode
Signed-off-by: Sage Weil <sage@redhat.com>

src/os/bluestore/BlueStore.h
@@ -639,6 +639,7 @@ private:
   void _assign_nid(TransContext *txc, OnodeRef o);
 
   void _dump_onode(OnodeRef o, int log_leverl=30);
+  void _dump_bnode(BnodeRef b, int log_leverl=30);
 
   TransContext *_txc_create(OpSequencer *osr);
   void _txc_release(TransContext *txc, CollectionRef& c, OnodeRef& onode,

wuxingyi/ceph-1

src/os/bluestore/BlueStore.h
@@ -642,6 +642,7 @@ private:
   void _assign_nid(TransContext *txc, OnodeRef o);
 
   void _dump_onode(OnodeRef o, int log_level=30);
+  void _dump_bnode(BnodeRef b, int log_level=30);
 
   TransContext *_txc_create(OpSequencer *osr);
   void _txc_release(TransContext *txc, CollectionRef& c, OnodeRef& onode,